6bc224b0857a6b107bec6563b6a16e1281327c8c
Aariv <arivbits@gmail.com>
2021-08-29 22:05:53 +0530

    Worked on Searching problem

--- GFG/src/main/java/com/ariv/gfg/easy/searching/_01LeftMostRightMostIndex.java
+++ GFG/src/main/java/com/ariv/gfg/easy/searching/_01LeftMostRightMostIndex.java
@@ -2,4 +2,35 @@ package com.ariv.gfg.easy.searching;
 
 public class _01LeftMostRightMostIndex {
 
+	public static void main(String[] args) {
+		int[] arr = {1, 3, 5, 5, 5, 5, 67, 123, 125};
+		int x = 5;
+		pair pair = indexes(arr, x);
+		System.out.println(pair.first);
+		System.out.println(pair.last);
+	}
+
+	private static pair indexes(int[] arr, int x) {
+		int first = -1, last = -1;
+		int n = arr.length;
+		for(int i = 0; i < n; ++i) {
+			if(arr[i] != x) {
+				continue;
+			}
+			if(first == -1)
+				first = i;
+			last = i;
+		}
+		return new pair(first, last);
+	}
+	
+}
+
+class pair {
+	long first;
+	long last;
+	public pair(long first, long last) {
+		this.first = first;
+		this.last = last;
+	}
 }
--- GFG/src/main/java/com/ariv/gfg/easy/searching/_02BitonicPoint.java
+++ GFG/src/main/java/com/ariv/gfg/easy/searching/_02BitonicPoint.java
@@ -2,4 +2,32 @@ package com.ariv.gfg.easy.searching;
 
 public class _02BitonicPoint {
 
+	public static void main(String[] args) {
+		int[] arr = {-3, -2, 4, 6, 10, 8, 7, 1};
+		int result = findMaximum(arr);
+		System.out.println(result);
+	}
+
+	private static int findMaximum(int[] arr) {
+		int n = arr.length;
+
+		int left = 1;
+		int right = n - 2;
+
+		while (left <= right) {
+			int mid = (left + right) / 2;
+
+			if (arr[mid - 1] < arr[mid + 1] && arr[mid] > arr[mid + 1]) {
+				return arr[mid];
+			}
+
+			// 
+			if(arr[mid] < arr[mid+1]) {
+				left = mid + 1;
+			} else {
+				right = mid - 1;
+			}
+		}
+		return -1;
+	}
 }
