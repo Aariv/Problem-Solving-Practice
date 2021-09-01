a87dc0c8278b869dee557132e4bf68fab53c1b84
Aariv <arivbits@gmail.com>
2021-09-01 19:45:03 +0530

    Worked on String problems

--- GFG/src/main/java/com/ariv/gfg/easy/string/_01ReverseWords.java
+++ GFG/src/main/java/com/ariv/gfg/easy/string/_01ReverseWords.java
@@ -1,16 +1,54 @@
 package com.ariv.gfg.easy.string;
 
+import java.util.Stack;
+
 public class _01ReverseWords {
 
 	public static void main(String[] args) {
 		String str = "i.like.this.program.very.much";
-		String strResult = reverseWords(str);
+		System.out.println(str);
+		String strResult = reverse(str);
 		// much.very.program.this.like.i
 		System.out.println(strResult);
 	}
 
 	private static String reverseWords(String str) {
 		StringBuilder sb = new StringBuilder();
+
+		return sb.toString();
+	}
+
+	// Function to reverse a text without reversing the individual words.
+	public static String reverse(String s) {
+		// `s[low…high]` forms a word
+		int low = 0, high = 0;
+
+		// create an empty stack
+		Stack<String> stack = new Stack<>();
+
+		// scan the text
+		for (int i = 0; i < s.length(); i++) {
+			// if `.` is found, we found a word
+			if (s.charAt(i) == '.') {
+				// push each word into the stack
+				stack.push(s.substring(low, high + 1));
+
+				// reset `low` and `high` for the next word
+				low = high = i + 1;
+			} else {
+				high = i;
+			}
+		}
+
+		// push the last word into the stack
+		stack.push(s.substring(low));
+
+		// construct the string by following the LIFO order
+		StringBuilder sb = new StringBuilder();
+		while (!stack.empty()) {
+			sb.append(stack.pop()).append('.');
+		}
+		sb.deleteCharAt(sb.length() - 1);// remove last space
 		return sb.toString();
 	}
 }
--- GFG/src/main/java/com/ariv/gfg/easy/string/_02LongestCommonPrefix.java
+++ GFG/src/main/java/com/ariv/gfg/easy/string/_02LongestCommonPrefix.java
@@ -2,4 +2,30 @@ package com.ariv.gfg.easy.string;
 
 public class _02LongestCommonPrefix {
 
+	public static void main(String[] args) {
+		String[] arr = { "geeksforgeeks", "geeks", "geek", "geezers" };
+		String result = longestCommonPrefix(arr);
+		System.out.println(result);
+	}
+
+	private static String longestCommonPrefix(String[] arr) {
+		String prefix = arr[0];
+		for(int i = 1; i < arr.length; ++i) {
+			prefix = findLCP(prefix, arr[i]);
+		}
+		return prefix;
+	}
+
+	private static String findLCP(String prefix, String string) {
+		StringBuilder sb = new StringBuilder();
+		int n1 = prefix.length();
+		int n2 = string.length();
+		for(int i = 0, j = 0; i < n1 && j < n2; i++, j++) {
+			if(prefix.charAt(i) != string.charAt(j)) {
+				break;
+			}
+			sb.append(prefix.charAt(i));
+		}
+		return sb.toString();
+	}
 }
