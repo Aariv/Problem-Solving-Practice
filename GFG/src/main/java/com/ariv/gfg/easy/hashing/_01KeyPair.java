39379e670811efb985fd09c1d620000e4770f6fd
Aariv <arivbits@gmail.com>
2021-09-04 15:20:25 +0530

    Worked on Hashing problems

--- GFG/src/main/java/com/ariv/gfg/easy/hashing/_01KeyPair.java
+++ GFG/src/main/java/com/ariv/gfg/easy/hashing/_01KeyPair.java
@@ -1,8 +1,6 @@
 package com.ariv.gfg.easy.hashing;
 
 import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
 
 public class _01KeyPair {
 
--- GFG/src/main/java/com/ariv/gfg/easy/hashing/_02TopKFrequentElement.java
+++ GFG/src/main/java/com/ariv/gfg/easy/hashing/_02TopKFrequentElement.java
@@ -1,5 +1,67 @@
 package com.ariv.gfg.easy.hashing;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.PriorityQueue;
+
 public class _02TopKFrequentElement {
 
+	public static void main(String[] args) {
+		int[] arr = { 1, 1, 2, 2, 4, 3, 3, 3 };
+		int k = 2;
+		topK(arr, k);
+	}
+
+	private static int[] topK(int[] arr, int k) {
+		Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+
+		for (int i = 0; i < arr.length; ++i) {
+			map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
+		}
+		List<Map.Entry<Integer, Integer>> list = new ArrayList<Map.Entry<Integer,Integer>>(map.entrySet());
+		
+		Collections.sort(list, new Comparator<Map.Entry<Integer, Integer>>() {
+
+			@Override
+			public int compare(Entry<Integer, Integer> o1, Entry<Integer, Integer> o2) {
+				if(o1.getValue() == o2.getValue()) {
+					return o2.getKey() - o1.getKey();
+				}
+				return o2.getValue() - o1.getValue();
+			}
+
+		});
+		int[] result = new int[k];
+		for(int i = 0; i < k; ++i) {
+			result[i] = list.get(i).getKey();
+		}
+		return result;
+	}
+	
+	private static void topKUsingPQ(int[] arr, int k) {
+		Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+
+		for (int i = 0; i < arr.length; ++i) {
+			map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
+		}
+		
+		PriorityQueue<Map.Entry<Integer, Integer>> queue = 
+				new PriorityQueue<Map.Entry<Integer,Integer>>(
+						(a, b) -> a.getValue().equals(b.getValue())
+						? Integer.compare(b.getKey(), a.getKey())
+					    : Integer.compare(b.getValue(), a.getValue()));
+		
+		for (Entry<Integer, Integer> entrySet : map.entrySet()) {
+			queue.offer(entrySet);
+		}
+		
+		for(int i = 0; i < k; ++i) {
+			System.out.println(queue.poll().getKey());
+		}
+	}
 }
