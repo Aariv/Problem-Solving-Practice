4c6a59dd078a6ae8d501c313510432b38d10e3af
Aariv <arivbits@gmail.com>
2021-09-04 21:02:24 +0530

    Worked on Hashing and Bit Masking

--- GFG/src/main/java/com/ariv/gfg/easy/bitmasking/_01ReverseBits.java
+++ GFG/src/main/java/com/ariv/gfg/easy/bitmasking/_01ReverseBits.java
@@ -2,4 +2,33 @@ package com.ariv.gfg.easy.bitmasking;
 
 public class _01ReverseBits {
 
+	public static void main(String[] args) {
+		int n = 11;
+		System.out.println(reverseBits(n));
+	}
+
+	private static void reverseBitsBF(int n) {
+		while (n != 0) {
+			System.out.print(n % 2);
+			n /= 2;
+		}
+		System.out.println();
+		while (n != 0) {
+			System.out.print(n % 2);
+			n /= 2;
+		}
+	}
+	
+	private static int reverseBits(int n) {
+		int rev = 0;
+		while (n > 0) {
+			rev <<= 1;
+			
+			if((int)(n&1) == 1)
+				rev ^= 1;
+			
+			n >>= 1;
+		}
+		return rev;
+	}
 }
--- GFG/src/main/java/com/ariv/gfg/easy/hashing/_03IntersectionOfTwoArrays.java
+++ GFG/src/main/java/com/ariv/gfg/easy/hashing/_03IntersectionOfTwoArrays.java
@@ -1,5 +1,73 @@
 package com.ariv.gfg.easy.hashing;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 public class _03IntersectionOfTwoArrays {
 
+	public static void main(String[] args) {
+		int[] arr1 = { 1, 2, 3, 4, 5, 6 };
+		int[] arr2 = { 3, 4, 5, 6, 7 };
+		int result = NumberofElementsInIntersection(arr1, arr2);
+		System.out.println(result);
+	}
+
+	private static int NumberofElementsInIntersection(int[] arr1, int[] arr2) {
+		Arrays.sort(arr1);
+		Arrays.sort(arr2);
+		int count = 0;
+		int i = 0, j = 0;
+		
+		while(i < arr1.length && j < arr2.length) {
+			if(arr1[i] > arr2[j])
+				j++;
+			else if(arr2[j] > arr1[i])
+				i++;
+			else {
+				count++;
+				i++;j++;
+			}
+		}
+		return count;
+	}
+
+	private static int NumberofElementsInUnion(int[] arr1, int[] arr2) {
+		Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+		for(int i =0; i < arr1.length; ++i) {
+			map.put(arr1[i], i);
+		}
+		for(int i =0; i < arr2.length; ++i) {
+			map.put(arr2[i], i);
+		}
+		for(Map.Entry<Integer, Integer> keyValue: map.entrySet()) {
+			
+		}
+		return 0;
+	}
+
+	private static int NumberofElementsInIntersectionBF(int[] arr1, int[] arr2) {
+//		Set<Integer> data1 = new HashSet<Integer>();
+//		for (int ele : arr1) {
+//			data1.add(ele);
+//		}
+//		int count = 0;
+//		for (int ele : arr2) {
+//			if (!data1.add(ele)) {
+//				++count;
+//			}
+//		}
+		int count = 0;
+		HashSet<Integer> hs = new HashSet<>();
+ 
+        for (int i = 0; i < arr1.length; i++)
+            hs.add(arr1[i]);
+ 
+        for (int i = 0; i < arr2.length; i++)
+            if (hs.contains(arr2[i]))
+                count++;
+		return count;
+	}
 }
