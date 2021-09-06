136c0c04eef3317383add6e5283f05e4eeba7045
Aariv <arivbits@gmail.com>
2021-09-06 21:19:00 +0530

    Worked on LinkedList problems

--- GFG/src/main/java/com/ariv/gfg/easy/linkedlist/_02ReverseLinkedList.java
+++ GFG/src/main/java/com/ariv/gfg/easy/linkedlist/_02ReverseLinkedList.java
@@ -2,4 +2,30 @@ package com.ariv.gfg.easy.linkedlist;
 
 public class _02ReverseLinkedList {
 
+	public static void main(String[] args) {
+		Node head = new Node(1);
+		head.next = new Node(2);
+		head.next.next = new Node(3);
+		head.next.next.next = new Node(4);
+		Node result = reverseLinkedList(head);
+		while (result != null) {
+			System.out.print(result.data + " ");
+			result = result.next;
+		}
+		System.out.println();
+	}
+
+	private static Node reverseLinkedList(Node head) {
+		Node temp = head;
+
+		Node prev = null;
+
+		while (temp != null) {
+			Node next = temp.next;
+			temp.next = prev;
+			prev = temp;
+			temp = next;
+		}
+		return prev;
+	}
 }
