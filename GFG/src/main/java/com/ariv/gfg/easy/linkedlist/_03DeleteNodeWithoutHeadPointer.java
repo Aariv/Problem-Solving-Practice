027da7092cd3a9c4eaf75b19cf9fe5106786772d
Aariv <arivbits@gmail.com>
2021-09-07 21:12:16 +0530

    Worked on LinkedList Problems

--- GFG/src/main/java/com/ariv/gfg/easy/linkedlist/_03DeleteNodeWithoutHeadPointer.java
+++ GFG/src/main/java/com/ariv/gfg/easy/linkedlist/_03DeleteNodeWithoutHeadPointer.java
@@ -2,4 +2,31 @@ package com.ariv.gfg.easy.linkedlist;
 
 public class _03DeleteNodeWithoutHeadPointer {
 
+	public static void main(String[] args) {
+		Node head = new Node(1);
+		head.next = new Node(2);
+		head.next.next = new Node(3);
+		head.next.next.next = new Node(4);
+		
+		display(head);
+		
+		deleteNode(head.next.next);
+		
+		display(head);
+	}
+
+	private static void display(Node head) {
+		Node temp = head;
+		
+		while(temp != null) {
+			System.out.print(temp.data + " ");
+			temp = temp.next;
+		}
+		System.out.println();
+	}
+
+	private static void deleteNode(Node node) {
+		node.data = node.next.data;
+		node.next = node.next.next;
+	}
 }
