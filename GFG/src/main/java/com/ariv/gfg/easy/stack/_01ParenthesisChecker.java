7c81f8aefbb284b3efb8213836c9d43b657edc8f
Aariv <arivbits@gmail.com>
2021-09-16 21:18:06 +0530

    Worked on a stack problem

--- GFG/src/main/java/com/ariv/gfg/easy/stack/_01ParenthesisChecker.java
+++ GFG/src/main/java/com/ariv/gfg/easy/stack/_01ParenthesisChecker.java
@@ -1,5 +1,38 @@
 package com.ariv.gfg.easy.stack;
 
+import java.util.Stack;
+
 public class _01ParenthesisChecker {
 
+	public static void main(String[] args) {
+		String s = "[({[([{}])]})}";
+		System.out.println(ispar(s)); // true
+	}
+
+	private static boolean ispar(String s) {
+		char[] arr = s.toCharArray();
+		Stack<Character> stack = new Stack<Character>();
+		
+		for(char ch: arr) {
+			if(ch == '{' || ch == '(' || ch == '[') {
+				stack.push(ch);
+			} else if(ch == '}' || ch == ')' || ch == ']') {
+				if(stack.isEmpty()) {
+					return false;
+				}
+				char pop = stack.pop();
+				if(ch == '}' && pop == '{') {
+					continue;
+				}
+				if(ch == ')' && stack.pop() == '(') {
+					continue;
+				}
+				if(ch == ']' && stack.pop() == '[') {
+					continue;
+				} else 
+					return false;
+			}
+		}
+		return stack.isEmpty();
+	}
 }
